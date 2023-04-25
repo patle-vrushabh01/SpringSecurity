e6f41c58357c9ab3c732a5b92d7cfd563bb3332f
vrushabh.patle <patle.vrushabh01@gmail.com>
2023-04-25 16:36:04 +0530

    updated role based auth

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,17 +10,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.STUDENT;
 
 @EnableWebSecurity
 @Configuration
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
      http.
              authorizeRequests()
-             .antMatchers("/","index","/css/*","/js/*")
-             .permitAll()
+             .antMatchers("/","index","/css/*","/js/*").permitAll()
+             .antMatchers("/api/**").hasRole(STUDENT.name())
              .anyRequest()
              .authenticated()
              .and()
@@ -30,7 +38,26 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails   annaSMith = User.builder().username("annasmit").password("passowrd").roles("STUDENT").build();
-        return new InMemoryUserDetailsManager(annaSMith);
+        UserDetails   annaSmithUser = User.builder().username("annasmit")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name()) // ROLE_ADMIN
+                .build();
+
+        /*UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                .roles("STUDENT") // ROLE_ADMINTRAINEE
+                .build();*/
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                lindaUser
+
+        );
     }
 }
