0af44f85222fbe512d4be05a9af0f4870c6aaa37
vrushabh.patle <patle.vrushabh01@gmail.com>
2023-05-05 17:27:48 +0530

    updated role based auth with permission based auth and preauth annotation

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserPermission.*;
 import static com.example.demo.security.ApplicationUserRole.*;
@@ -28,10 +32,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
      http
-   //     .csrf().disable()//this we did bcz put/post method giving us 403 forbidden
+       .csrf().disable()//this we did bcz put/post method giving us 403 forbidden
 
         .authorizeRequests()
-             .antMatchers("/","index","/css/*","/js/*").permitAll()
+             .antMatchers("/","index","/css/*","/js/*","/login").permitAll()
              .antMatchers("/api/**").hasRole(STUDENT.name())
        //      .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
       //       .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
@@ -40,7 +44,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
              .anyRequest()
              .authenticated()
              .and()
-             .httpBasic();
+                .formLogin()
+                 .loginPage("/login")  //load the login page
+                 .permitAll()
+                .defaultSuccessUrl("/courses",true) //force the request to redirect to this page after successful login
+                .usernameParameter("username") //if we want to modify the form filed then me can modify them in html and pass the same filed name over here
+                .passwordParameter("password") //for both username and password param
+             .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(30)) //it will make the application to remember the user for 2 weeks
+                    .key("validate key md5")
+                    .rememberMeParameter("remember-me") //we can modify this as well
+             .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET")) //this we are using bcz csrf is disabled if csrf is enabled then use post
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
 
     }
 
